d9d4aa212a933c3c47e2a460dc040416f21d1be9
csyler <egorgarderobov@gmail.com>
2025-03-28 14:14:34 +0300

    return values

--- src/script.js
+++ src/script.js
@@ -1,11 +1,3 @@
 function  hello (argument) {
 	console.log("Hello World");
 }
-
-function  goodbye(argument) {
-	console.log("goodbye");
-}
-
-function  thanks(argument) {
-	console.log("Something for work");
-}
